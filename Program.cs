f0cf87d139251881a97e592aaa6f0a4981bec3f5
goyal123 <akhilg642@gmail.com>
2022-07-08 13:29:00 +0530

    Final commit

--- Program.cs
+++ Program.cs
@@ -5,12 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
             Console.WriteLine("Hello New, World!");
-            
-=======
            
->>>>>>> dd069ceb71e57e7855e14ba1028c5b6ce3985d9f
         }
     }
 }

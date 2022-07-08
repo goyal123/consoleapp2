dd069ceb71e57e7855e14ba1028c5b6ce3985d9f
Akhil Goyal <akhilg642@gmail.com>
2022-07-08 13:22:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello New, World!");
-            Console.WriteLine("Hello remote, World!");
+           
         }
     }
 }
